5a11b8db5eb01502c413e8ea40483b18049e46ec
hyojeong <khj444453@gmail.com>
2023-01-19 11:05:45 +0900

    webhook test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("args = " + args);
+		System.out.println("webhook test");
 	}
 	
 }
